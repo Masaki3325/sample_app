d974badef4535007b5cc960ed9bc1dfba3c7b970
Masaki3325 <masakino735@gmail.com>
2024-08-19 18:18:53 +0900

    refactring settings

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
 bundle install
-yarn install
-yarn build # jsファイルをesbuildでバンドルしているため
 bundle exec rake assets:precompile # cssはsprocketsを使っているため
 bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile #
