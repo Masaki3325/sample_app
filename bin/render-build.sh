4929eb4613ce2c3bd1dfa1f25aea88b723ec6232
Masaki3325 <masakino735@gmail.com>
2024-08-19 18:45:30 +0900

    refactoring settings for render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
 bundle install
-bundle exec rake assets:precompile # cssはsprocketsを使っているため
-bundle exec ridgepole -c config/database.yml -E production --apply -f db/schemas/Schemafile #
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
