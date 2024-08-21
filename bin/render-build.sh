d06f842fe493d3980c4414516fd25307ce3a226d
Masaki3325 <masakino735@gmail.com>
2024-08-21 21:00:50 +0900

    update render file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,7 @@
 # exit on error
 set -o errexit
 
-yarn cache clean
 bundle install
-bundle exec rake assets:precompile
+bundle exec rails assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
