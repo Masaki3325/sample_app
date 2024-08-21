2ea854baac11af7e2277e225bb210e6c67bed5ad
Masaki3325 <masakino735@gmail.com>
2024-08-21 23:25:26 +0900

    update environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,26 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 
+// ProvidePluginの設定
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
   Popper: ['popper.js', 'default']
 }))
 
-module.exports = environment
+// nodeオプションの設定を追加
+environment.config.merge({
+  node: {
+    __dirname: false,    // 必要に応じて設定
+    __filename: false,   // 必要に応じて設定
+    global: true,        // 必要に応じて設定
+    // 以下のプロパティはWebpack 5ではサポートされていないため削除または無効化
+    // fs: 'empty',        // 削除または無効化
+    // net: 'empty',       // 削除または無効化
+    // tls: 'empty',       // 削除または無効化
+    // dgram: 'empty',     // 削除または無効化
+    // child_process: 'empty' // 削除または無効化
+  }
+});
+
+module.exports = environment;
