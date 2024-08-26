123440e1d768f64de698f534b8e0ba861f745e19
Masaki3325 <masakino735@gmail.com>
2024-08-26 18:24:24 +0900

    update file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,8 @@
 # exit on error
 set -o errexit
 
+rm -rf node_modules                                                                                                                                           9s Py base 17:43:37
+yarn install
 bundle install
 
 bundle exec rails assets:precompile
